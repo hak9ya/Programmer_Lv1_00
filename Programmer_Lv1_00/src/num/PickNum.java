919c3eb9c9c8b627c9f396eb1b2ea963eba3c40d
hak9ya <jhy2297@naver.com>
2021-03-03 19:14:45 +0900

    Programmers Test

--- Programmer_Lv1_00/src/num/PickNum.java
+++ Programmer_Lv1_00/src/num/PickNum.java
@@ -1,10 +1,39 @@
 package num;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class PickNum {
-
+	
 	public static void main(String[] args) {
+		int[] number1 = {2, 1, 3, 4, 1};
+		int[] number2 = {5, 0, 2, 7};
 		
-
+		int[] answer1 = solution(number1);
+		int[] answer2 = solution(number2);
+		
+		System.out.println(Arrays.toString(answer1));
+		System.out.println(Arrays.toString(answer2));
+		
+	}	
+	
+	private static int[] solution(int[] numbers) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+        for(int i = 0; i < numbers.length; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                if(!arr.contains(numbers[i] + numbers[j])) {
+                	arr.add(numbers[i] + numbers[j]);
+                }
+            }
+        }
+    
+		Collections.sort(arr);
+    	int[] answer = new int[arr.size()];
+    	
+    	for(int i = 0; i < arr.size(); i++) {
+    		answer[i] = arr.get(i);
+    	}
+		return answer;
 	}
-
 }
